c5fa2a2fe9a1d13ecfed76996c571a601c8a4eec
heitorsdev <heitorhilles@gmail.com>
2026-02-09 12:12:27 -0300

    constants

---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems.Shooter;
+
+public class ShooterConstants {
+    public static int right_shooter_id = 0; //flex
+    public static int left_shooter_id = 0; //flex
+    public static int right_hood_id = 0; //max
+    public static int left_hood_id = 0; //max
+
+    public static double shooterkP = 0.0;
+    public static double shooterkI = 0.0;
+    public static double shooterkD = 0.0;
+    public static double shooterkV = 0.0;
+
+    
+    public static double hoodkP = 0.0;
+    public static double hoodkI = 0.0;
+    public static double hoodkD = 0.0;
+    
+
+
+    
+
+
+}
