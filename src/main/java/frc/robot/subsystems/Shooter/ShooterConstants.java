cb14b83ea51a3be65f8e199d5b7cbcb722d172fc
heitorsdev <heitorhilles@gmail.com>
2026-02-10 11:03:51 -0300

    removed all hood references (it doesnt exist anymor)

--- src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
@@ -5,18 +5,12 @@ import edu.wpi.first.math.geometry.Pose2d;
 public class ShooterConstants {
     public static int right_shooter_id = 0; //flex
     public static int left_shooter_id = 0; //flex
-    public static int right_hood_id = 0; //max
-    public static int left_hood_id = 0; //max
 
     public static double shooterkP = 0.0;
     public static double shooterkI = 0.0;
     public static double shooterkD = 0.0;
     public static double shooterkV = 0.0;
 
-    
-    public static double hoodkP = 0.0;
-    public static double hoodkI = 0.0;
-    public static double hoodkD = 0.0;
     
 
     public static Pose2d redHubPose = new Pose2d();
