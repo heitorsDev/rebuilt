45b7bd3bb46e08f8bd95352a98740bd978187fd1
heitorsdev <heitorhilles@gmail.com>
2026-02-19 10:57:39 -0300

    climber subsystem

--- src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
@@ -1,11 +1,11 @@
 package frc.robot.subsystems.Climber;
 
 public class ClimberConstants {
-    public int climber_id = 0;
-    public int up_setpoint = 0;
-    public int down_setpoint = 0;
-    public double kP = 0.1;
-    public double kI = 0;
-    public double kD = 0;
+    public static int climber_id = 0;
+    public static double up_setpoint = 0;
+    public static double down_setpoint = 0;
+    public static double kP = 0.1;
+    public static double kI = 0;
+    public static double kD = 0;
 
 }
