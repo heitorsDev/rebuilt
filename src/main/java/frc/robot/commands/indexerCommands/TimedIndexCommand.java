25e4fe39601f194045228ec70e438785cc4c5774
heitorsDev <heitorhilles@gmail.com>
2026-02-19 23:45:58 -0300

    timed index command w autoalign

--- src/main/java/frc/robot/commands/indexerCommands/TimedIndexCommand.java
+++ src/main/java/frc/robot/commands/indexerCommands/TimedIndexCommand.java
@@ -2,16 +2,19 @@ package frc.robot.commands.indexerCommands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.swerveCommands.AlignToGoal;
 import frc.robot.subsystems.Indexer.Indexer;
+import frc.robot.subsystems.swerve.SwerveSubsystem;
 
 public class TimedIndexCommand extends SequentialCommandGroup{
-    public TimedIndexCommand(Indexer indexer, double time){
+    public TimedIndexCommand(Indexer indexer, SwerveSubsystem swerve, double time){
 
         addCommands(
+            new AlignToGoal(swerve),
             new IndexCommand(indexer),
             new WaitCommand(time),
             new DeIndexCommand(indexer)
         );
-        addRequirements(indexer);
+        addRequirements(indexer, swerve);
     }
 }
